648e53110c898b7dd22c6d9bd93ab481e2f24ac0
허승준 <juniheo@wirelessprv-10-194-178-105.near.illinois.edu>
2022-03-11 13:30:58 -0600

    updateJh

--- Research/heo11/t1/test.py
+++ Research/heo11/t1/test.py
@@ -7,8 +7,10 @@ app = Flask(__name__)
 def temperature():
     zipcode = request.form["zip"]
     r = requests.get("http://api.openweathermap.org/data/2.5/weather?zip="+zipcode+",us&appid=192fba108273b0985d9f580e8ad91e0f")
-    json_object = r.text
-    return json_object
+    json_object = r.json
+    temp_k = float(json_object['main']['temp'])
+    temp_f = (temp_k - 273.15) * 1.8 + 32
+    return render_template('temperature.html', temp = temp_f)
 
 @app.route("/")
 def index():
