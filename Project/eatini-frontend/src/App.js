db734ae41ac46479eadf5364aa67d4a8c416e72b
lycheebbe <99426353+lycheebbe@users.noreply.github.com>
2022-04-29 17:04:58 -0500

    :add first col component, have a basic look of the first component in a scoll-down list

--- Project/eatini-frontend/src/App.js
+++ Project/eatini-frontend/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import Rlist from "./Components/Rlist"
-
+import Rlist from "./Components/Rlist";
 
 
 function App() {
   return (
     <div className="App">
       <div className = "content">
+        {/* <Rlist title="name"></Rlist>
+        <Rlist typeOfR="type"></Rlist> */}
         <Rlist />
       </div>
     </div>
--- Project/eatini-frontend/src/Components/Rlist.js
+++ Project/eatini-frontend/src/Components/Rlist.js
@@ -1,21 +1,135 @@
 import React from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 
-const Rlist = () => {
+// title
+var Rtitle = "title";
+
+// type
+var Rtype = "type";
+
+//rating
+var Rrating = "rating";
+
+// average cost
+var Racost = "cost";
+
+// location
+var Rloc = "location";
+
+// website
+var Rweb = "url";
+
+// from-to, need tostring or something else to turn int to string
+var FromToT = "time";
+var FromToM = "dis";
+
+// to-to, need tostring or something else to turn int to string
+var ToToT = "time";
+var ToToM = "dis";
+
+export default function Test() {
     return (
-        <Container>
-            <Row>
-                <Col>1 of 3</Col>
-                <Col xs={6}>2 of 3 (wider)</Col>
-                <Col>3 of 3</Col>
-            </Row>
-            <Row>
-                <Col>1 of 3</Col>
-                <Col xs={5}>2 of 3 (wider)</Col>
-                <Col>3 of 3</Col>
-            </Row>
-        </Container>
-    );
-}
+      <div style={{ display: 'block', width:"400", padding: 20 }}>
+        <h4>React-Bootstrap Col Component</h4>
+        <Row>
+          <Col style={{
+            width: "900",
+            padding: 20,
+            backgroundColor: 'pink',
+          }}>
+            <Rtitle /> 
+            title/name
+        </Col>
+        </Row>
 
-export default Rlist;
+        <Row>
+          <Col style={{
+            width: "200",
+            backgroundColor: 'lightblue',
+          }}>
+            <Rtype />
+            type
+        </Col>
+          <Col style={{
+            width: "500",
+            backgroundColor: 'lightgreen',
+          }}>
+            <Rrating />
+            rating
+        </Col>
+          <Col style={{
+            width: "500",
+            backgroundColor: 'violet',
+          }}>
+            <Racost />
+            average cost
+        </Col>
+        </Row>
+
+        <Row>
+          <Col style={{
+            width: "500",
+            backgroundColor: 'orange',
+          }}>
+            🔻 Location: <Rloc />
+          </Col>
+        </Row>
+
+        <Row>
+          <Col style={{
+            width: "500",
+            backgroundColor: 'lightyellow',
+          }}>
+            📋 Webiste: <Rweb />
+          </Col>
+        </Row> 
+
+        <Row>
+          <Col style={{
+            width: "500",
+            backgroundColor: 'lightyellow',
+          }}>
+            ⏰ From To: <FromToM /> min, <FromToM /> mile
+          </Col>
+        </Row>
+
+        <Row>
+          <Col style={{
+            width: "500",
+            backgroundColor: 'lightyellow',
+          }}>
+            ⏰ To To: <FromToM /> min, <FromToM /> mile
+          </Col>
+        </Row>
+
+      </div>
+    );
+        }
+
+// const Rlist = () => {
+//     return (
+
+//         <Container>
+//             <Row>
+//                 <Col> {props.title}</Col>
+//             </Row>
+//         </Container>
+//     );
+// }
+
+// export default function Rlist(props) {
+//     return (
+// <div>
+//     {props.title}
+//     {props.typeOfR}
+//     {props.rating}
+//     {props.averageCost}
+//     {props.Location}
+//     {props.Wbesite}
+//     {props.fromTo}
+//     {props.ToTo}
+// </div>
+//     );
+// };
