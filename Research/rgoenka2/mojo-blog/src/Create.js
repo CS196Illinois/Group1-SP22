8eb78a2a34e5ce8121d2a026bf9a6e9320e437fd
ram <ramgoennka08@gmail.com>
2022-04-29 19:52:52 -0500

    moving form code to project folder

---
+++ Research/rgoenka2/mojo-blog/src/Create.js
@@ -0,0 +1,24 @@
+const Create = () => {
+    return ( 
+    <div className="create">
+        <h2>Add a New Blog</h2>
+        <form>
+            <label>Blog title:</label>
+            <input 
+            type="test"
+            />
+            <lable>Blog body:</lable>
+            <textarea
+            required
+            ></textarea>
+            <label>Blog author:</label>
+            <select>
+                <option value="mario">mario</option>
+                <option value="yoshi">yoshi</option>
+            </select>
+            </form>
+        </div>   
+    );
+}
+ 
+export default Create;
